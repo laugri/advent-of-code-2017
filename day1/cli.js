5dcf1d1ad0c096fdb61fdaaeacfe2ca82d8e5030
Christophe Hurpeau <christophe@hurpeau.com>
2017-12-09 11:51:32 +0100

    feat: add day1 cli

---
+++ day1/cli.js
@@ -0,0 +1,13 @@
+import readline from 'readline';
+import solveCaptcha from './index';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question('Input ?', input => {
+  // eslint-disable-next-line no-console
+  console.log(solveCaptcha(input));
+});
