92d84ba6d10a0dd13cb035d709ffe9058c6eb806
Laurent Grima <grima.laurent@gmail.com>
2017-12-09 12:11:55 +0100

    fix: add close to cli for day 1

--- day1/cli.js
+++ day1/cli.js
@@ -3,11 +3,12 @@ import solveCaptcha from './index';
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
-
 
 rl.question('Input ?', input => {
   // eslint-disable-next-line no-console
   console.log(solveCaptcha(input));
+  rl.close();
 });
+
