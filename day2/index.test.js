0b9ebdc1ea7cbc4f3a0af02a7713cb58b4f443d1
Christophe Hurpeau <christophe@hurpeau.com>
2017-12-09 12:05:53 +0100

    test: add 2 more tests on diffBetweenLargestSmallestValue

--- day2/index.test.js
+++ day2/index.test.js
@@ -12,4 +12,12 @@ describe('diffBetweenLargestSmallestValue', () => {
   test("The first row's largest and smallest values are 9 and 1, and their difference is 8.", () => {
     expect(diffBetweenLargestSmallestValue('5 1 9 5')).toBe(8);
   });
+
+  test('The second row\'s largest and smallest values are 7 and 3, and their difference is 4.', () => {
+    expect(diffBetweenLargestSmallestValue('7 5 3')).toBe(4);
+  });
+
+  test('The third row\'s difference is 6.', () => {
+    expect(diffBetweenLargestSmallestValue('2 4 6 8')).toBe(6);
+  });
 });
